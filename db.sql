0a91aff4c916204876544aaafd4fd247fd3ade25
Anthony HUYNH <anthonyhuynh1204@gmail.com>
2024-05-07 14:54:47 +0200

    fix: name change on db

--- db.sql
+++ db.sql
@@ -1,63 +1,63 @@
 CREATE DATABASE simplimmo;
 USE simplimmo;
-CREATE TABLE type_biens (
+CREATE TABLE type_property (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(50) NOT NULL
+    name VARCHAR(50) NOT NULL
 );
 CREATE TABLE types_transactions (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(50) NOT NULL
+    name VARCHAR(50) NOT NULL
 );
 CREATE TABLE regions (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100) NOT NULL
+    name VARCHAR(100) NOT NULL
 );
-CREATE TABLE villes (
+CREATE TABLE city (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     id_regions INT,
     FOREIGN KEY (id_regions) REFERENCES regions(id)
 );
-CREATE TABLE user (
+CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100) NOT NULL,
-    prenom VARCHAR(100) NOT NULL,
+    firstname VARCHAR(100) NOT NULL,
+    lastnam VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    mot_de_passe VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     is_admin TINYINT(1) NOT NULL DEFAULT 0
 );
-CREATE TABLE photos (
+CREATE TABLE images (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    chemin VARCHAR(255) NOT NULL
+    path VARCHAR(255) NOT NULL
 );
-CREATE TABLE annonces (
+CREATE TABLE property (
     id INT PRIMARY KEY AUTO_INCREMENT,
     id_user INT DEFAULT NULL,
-    id_type_bien INT,
+    id_type_property INT,
     id_type_transaction INT,
-    id_ville INT,
+    id_city INT,
     surface DECIMAL(10, 2) NOT NULL,
     prix DECIMAL(10, 2) NOT NULL,
     description TEXT,
     date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_user) REFERENCES users(id),
-    FOREIGN KEY (id_type_bien) REFERENCES type_biens(id),
+    FOREIGN KEY (id_type_property) REFERENCES type_property(id),
     FOREIGN KEY (id_type_transaction) REFERENCES types_transactions(id),
-    FOREIGN KEY (id_ville) REFERENCES villes(id)
+    FOREIGN KEY (id_city) REFERENCES city(id)
 );
-CREATE TABLE annonce_photos (
-    id_annonce INT,
-    id_photo INT,
-    PRIMARY KEY (id_annonce, id_photo),
-    FOREIGN KEY (id_annonce) REFERENCES annonces(id),
+CREATE TABLE property_imagess (
+    id_property INT,
+    id_images INT,
+    PRIMARY KEY (id_property, id_images),
+    FOREIGN KEY (id_property) REFERENCES property(id),
     FOREIGN KEY (id_photo) REFERENCES photos(id)
 );
-CREATE TABLE demandes_rappel (
+CREATE TABLE call_requests (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_annonces INT,
+    id_property INT,
     id_user INT,
-    email_visiteur VARCHAR(255) NOT NULL,
-    date_heure_rappel DATETIME NOT NULL,
-    FOREIGN KEY (id_annonces) REFERENCES annonces(id),
+    email_guest VARCHAR(255) NOT NULL,
+    date_time_recall DATETIME NOT NULL,
+    FOREIGN KEY (id_property) REFERENCES property(id),
     FOREIGN KEY (id_user) REFERENCES users(id)
 );
