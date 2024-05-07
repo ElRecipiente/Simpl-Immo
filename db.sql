4e5b8a81561420ad2a92ffe6c44dc3dd83c1c551
Anthony HUYNH <anthonyhuynh1204@gmail.com>
2024-05-07 14:58:25 +0200

    fix: fix some stuff in db

--- db.sql
+++ db.sql
@@ -21,7 +21,7 @@ CREATE TABLE city (
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     firstname VARCHAR(100) NOT NULL,
-    lastnam VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     is_admin TINYINT(1) NOT NULL DEFAULT 0
@@ -32,15 +32,15 @@ CREATE TABLE images (
 );
 CREATE TABLE property (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_user INT DEFAULT NULL,
+    id_users INT DEFAULT NULL,
     id_type_property INT,
     id_type_transaction INT,
     id_city INT,
     surface DECIMAL(10, 2) NOT NULL,
-    prix DECIMAL(10, 2) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
     description TEXT,
     date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_user) REFERENCES users(id),
+    FOREIGN KEY (id_users) REFERENCES users(id),
     FOREIGN KEY (id_type_property) REFERENCES type_property(id),
     FOREIGN KEY (id_type_transaction) REFERENCES types_transactions(id),
     FOREIGN KEY (id_city) REFERENCES city(id)
@@ -50,14 +50,14 @@ CREATE TABLE property_imagess (
     id_images INT,
     PRIMARY KEY (id_property, id_images),
     FOREIGN KEY (id_property) REFERENCES property(id),
-    FOREIGN KEY (id_photo) REFERENCES photos(id)
+    FOREIGN KEY (id_images) REFERENCES images(id)
 );
 CREATE TABLE call_requests (
     id INT PRIMARY KEY AUTO_INCREMENT,
     id_property INT,
-    id_user INT,
+    id_users INT,
     email_guest VARCHAR(255) NOT NULL,
     date_time_recall DATETIME NOT NULL,
     FOREIGN KEY (id_property) REFERENCES property(id),
-    FOREIGN KEY (id_user) REFERENCES users(id)
+    FOREIGN KEY (id_users) REFERENCES users(id)
 );
