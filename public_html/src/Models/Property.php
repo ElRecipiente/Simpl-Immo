3174d45c40d49402f8113687ceb8ead23683eba8
Nicolas Droux <nicolas.droux.2b@gmail.com>
2024-05-14 14:53:11 +0200

    create Property & PropertyRepository class

--- public_html/src/Models/Property.php
+++ public_html/src/Models/Property.php
@@ -3,15 +3,81 @@
 namespace Models;
 
 use core\DBConfig;
+
+/**
+ * Define Property class
+ */
 class Property extends DBConfig {
 
-    public function __construct()
-    {
-        $this->getConnection();
-    }
+    /**
+     * @var float
+     */
+    protected float $surface_area;
+    /**
+     * @var float
+     */
+    protected float $price;
+    /**
+     * @var string
+     */
+    protected string $description;
+    /**
+     * @var string
+     */
+    protected string $type_transaction;
+    /**
+     * @var string
+     */
+    protected string $type_property;
 
-    public function createProperty()
-    {
+    /**
+     * @return float
+     */
+public function getSurfaceArea() {
+    return $this->surface_area;
+}
 
-    }
+    /**
+     * @return float
+     */
+public function getPrice() {
+    return $this->price;
+}
+
+    /**
+     * @return string
+     */
+public function getDescription() {
+    return $this->description;
+}
+
+    /**
+     * @param $description
+     * @return void
+     */
+public function setDescription($description) {
+    $this->description = $description;
+}
+
+    /**
+     * @return string
+     */
+public function getTypeTransaction() {
+    return $this->type_transaction;
+}
+
+    /**
+     * @return string
+     */
+public function getTypeProperty() {
+    return $this->type_property;
+}
+
+    /**
+     * @return void
+     */
+public function create() {
+    // todo in child class
+}
+
 }
--- public_html/src/Repositories/PropertyRepository.php
+++ public_html/src/Repositories/PropertyRepository.php
@@ -2,8 +2,53 @@
 
 namespace Repositories;
 
-use Models\Property;
+use core\DBConfig;
+use PDO;
 
-class PropertyRepository extends Property {
+class PropertyRepository extends DBConfig {
 
+    private $table = "properties";
+
+    public function __construct()
+    {
+        $this->getConnection();
+    }
+
+    public function getAll()
+    {
+        $sql = "SELECT * FROM " . $this->table;
+        $query = $this->_connexion->prepare($sql);
+        $query->execute();
+        return $query->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function getOneById($id)
+    {
+        $sql = "SELECT * FROM " . $this->table . " WHERE id = :id";
+        $query = $this->_connexion->prepare($sql);
+        $query->bindParam(':id', $id);
+        $query->execute();
+        return $query->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function filterBySurfaceArea($surfaceArea){
+        $sql = "SELECT * FROM " . $this->table . " WHERE surfaceArea = :surfaceArea";
+        $query = $this->_connexion->prepare($sql);
+        $query->bindParam(':surfaceArea', $surfaceArea);
+        $query->execute();
+        return $query->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function filterByPrice($price){
+        $sql = "SELECT * FROM " . $this->table . " WHERE price = :price";
+        $query = $this->_connexion->prepare($sql);
+        $query->bindParam(':price', $price);
+        $query->execute();
+        return $query->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function create()
+    {
+
+    }
 }
