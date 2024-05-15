6ab49b2ebf9713060863851d53649d17e4e422a7
Nicolas Droux <nicolas.droux.2b@gmail.com>
2024-05-15 09:17:52 +0200

    start working on controller

--- public_html/public/index.php
+++ public_html/public/index.php
@@ -1,44 +1,4 @@
 <?php
 require_once "../vendor/autoload.php";
 
-use App\controller\PropertyController;
-use App\controller\HouseController;
-
-$bien_1 = new PropertyController("80", "120", "Ici", "2");
-//$db = new DBConfig();
-//var_dump($db->getConnection());
-$bien_1->create();
-
-// var_dump($bien_1);
-
-// echo "La surface est de " . $bien_1->getSurface() . " mètres carrés.";
-
-$maison_1 = new HouseController(
-        "200",
-        "1200",
-        "làbas",
-        "5",
-        "aucun",
-        0,
-        3
-);
-
-function debug($arg)
-{
-    echo "<pre>" . print_r($arg, true) . "</pre>";
-}
-
-debug($maison_1);
-
-?>
-
-<html>
-
-<p>Le tarif hors frais est de <?php echo $bien_1->getPrix()?> euros.</p>
-<p>Le tarif avec les frais de notaire est de <?php echo $bien_1->getPrixFNI()?> euros.</p>
-
-<?php $bien_1->setFraisDeNotaire(10) ?>
-
-<p>Le nouveau tarif avec frais de notaire est de <?php echo $bien_1->getPrixFNI() ?> euros.</p>
-
-</html>
+echo "hello";
--- public_html/src/Controllers/HouseController.php
+++ public_html/src/Controllers/HouseController.php
@@ -4,19 +4,5 @@ namespace Controllers;
 
 class HouseController extends PropertyController
 {
-    /**
-     * @param null $terrain
-     * @param bool $isPiscine
-     * @param int $etage
-     */
-    public function __construct($surface, $prix, $localisation, $nb_piece, private $terrain, private $isPiscine, private $etage)
-    {
-        parent::__construct($surface, $prix, $localisation, $nb_piece);
-    }
-
-    public function modifierPrix()
-    {
-        $nouveauPrix = $this->prix * 2;
-    }
 
 }
