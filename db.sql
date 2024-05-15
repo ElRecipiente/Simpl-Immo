58721af4b4c9bd1d2d75d56e635718833e9d4290
Nicolas Droux <nicolas.droux.2b@gmail.com>
2024-05-15 14:42:10 +0200

    fix: db

--- db.sql
+++ db.sql
@@ -1,17 +1,17 @@
 USE simplimmo;
 CREATE TABLE regions (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
 );
 CREATE TABLE cities (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     postal_code VARCHAR(20) NOT NULL,
     region_id INT,
-    FOREIGN KEY (region_id) REFERENCES Regions(id)
+    FOREIGN KEY (region_id) REFERENCES regions(id)
 );
 CREATE TABLE owners (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
     phone_number INT NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE owners (
     mail VARCHAR(255) NOT NULL
 );
 CREATE TABLE users (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     firstname VARCHAR(50) NOT NULL,
@@ -29,12 +29,12 @@ CREATE TABLE users (
     is_admin BOOLEAN NOT NULL DEFAULT 0
 );
 CREATE TABLE call_requests (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES Users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 CREATE TABLE properties (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     surface_area DECIMAL(10, 2) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     description TEXT NOT NULL,
@@ -42,36 +42,36 @@ CREATE TABLE properties (
     type_property VARCHAR(50) NOT NULL,
     city_id INT,
     owner_id INT,
-    FOREIGN KEY (city_id) REFERENCES Cities(id),
-    FOREIGN KEY (owner_id) REFERENCES Owners(id)
+    FOREIGN KEY (city_id) REFERENCES cities(id),
+    FOREIGN KEY (owner_id) REFERENCES owners(id)
 );
 CREATE TABLE photos (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     url VARCHAR(255) NOT NULL,
     alt VARCHAR(100) NOT NULL,
     property_id INT,
-    FOREIGN KEY (property_id) REFERENCES Properties(id)
+    FOREIGN KEY (property_id) REFERENCES properties(id)
 );
 CREATE TABLE garages (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     type VARCHAR(50) NOT NULL,
     underground BOOLEAN NOT NULL,
     property_id INT,
-    FOREIGN KEY (property_id) REFERENCES Properties(id)
+    FOREIGN KEY (property_id) REFERENCES properties(id)
 );
 CREATE TABLE houses (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     room_number INT NOT NULL,
     bedroom_number INT NOT NULL,
     garden_size DECIMAL(10, 2) NOT NULL,
     property_id INT,
-    FOREIGN KEY (property_id) REFERENCES Properties(id)
+    FOREIGN KEY (property_id) REFERENCES properties(id)
 );
 CREATE TABLE appartments (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     room_number INT NOT NULL,
     bedroom_number INT NOT NULL,
     garden BOOLEAN NOT NULL,
     property_id INT,
-    FOREIGN KEY (property_id) REFERENCES Properties(id)
+    FOREIGN KEY (property_id) REFERENCES properties(id)
 );
